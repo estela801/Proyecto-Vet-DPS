011d7d3d1dc18a14b738dd64dcb1ae9d5964308b
kfuentes466 <36283211+kfuentes466@users.noreply.github.com>
2020-10-07 20:05:15 -0600

    Avance del index del sitio

--- ProyectoDPS/src/app/app-routing.module.ts
+++ ProyectoDPS/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './componentes/index/index.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'/inicio',pathMatch:'full'},
+  {path:'inicio', component: IndexComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
