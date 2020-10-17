b3ce128ec246f28525105e03a3bdd08846c1e18f
kfuentes466 <36283211+kfuentes466@users.noreply.github.com>
2020-10-17 07:21:24 -0600

    Menu con funciones en el ts

--- ProyectoDPS/src/app/componentes/menu/menu.component.ts
+++ ProyectoDPS/src/app/componentes/menu/menu.component.ts
@@ -7,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
+  submenu1 : boolean = false;
+  submenu2 : boolean = false;
+  submenu3 : boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
     
   }
 
+  onsubmenu1(){
+    if(this.submenu1) this.submenu1 = false;
+    else this.submenu1 = true; 
+    this.submenu2 = false;
+    this.submenu3 = false;
+  }
+
+  onSubmenu2(){
+    if(this.submenu2) this.submenu2 = false;
+    else this.submenu2 = true;
+    this.submenu1 = false;
+    this.submenu3 = false;
+  }
+
+  onSubmenu3(){
+    if(this.submenu3) this.submenu3 = false;
+    else this.submenu3 = true;
+    this.submenu2 = false;
+    this.submenu1 = false;
+  }
 }
--- ProyectoDPS/src/app/componentes/menu/menu.js
+++ ProyectoDPS/src/app/componentes/menu/menu.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
            $(this).removeClass('activado');
            $(this).children('ul').slideUp();
         } else{
-            $('.menu li ul').slideUp();
+            $('.menu li  ul').slideUp();
             $('.menu li').removeClass('activado');
             $(this).addClass('activado');
             $(this).children('ul').slideDown();
