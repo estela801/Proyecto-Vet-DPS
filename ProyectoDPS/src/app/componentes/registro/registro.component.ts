423628001e4485b048bfb00329f415a518e15021
unknown <ros98cg98@gmail.com>
2020-10-19 00:16:48 -0600

    Insert Funcionando

--- ProyectoDPS/src/app/componentes/registro/registro.component.ts
+++ ProyectoDPS/src/app/componentes/registro/registro.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+
 import {Mascota} from './../../modelos/mascotas/mascota';
 import  {RegistroService} from '../../servicios/mascotas/registro.service';
 import Swal from 'sweetalert2';
-//import {MatSnackBar } from '@angular/material/snack-bar';
 import {Router} from '@angular/router';
-import { NullTemplateVisitor } from '@angular/compiler';
 
 
 @Component({
@@ -14,37 +13,22 @@ import { NullTemplateVisitor } from '@angular/compiler';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
- // title = 'newMat';
      
   isLinear = true;
- Propietario: FormGroup;
-Mascota: FormGroup;
-mascotaModel =
-{
-  nombre:null,
-  registro_mascota:null,
-  edad: null,
-  tipo: null,
-  raza: null,
-  sexo:null
-}
+ //Propietario: FormGroup;
+rmascota: FormGroup;
+mascotaModel= new Mascota()
+
+
 
   constructor(private _formBuilder: FormBuilder, private mascotasServicio: RegistroService,
    public router: Router,  ) { }
 
   ngOnInit() {
-    /*this.Propietario = this._formBuilder.group({
-      dui: ['', Validators.required],
+   
+    this.rmascota = this._formBuilder.group({
       nombre: ['', Validators.required],
-      apellido: ['', Validators.required],
-      telefono: ['', Validators.required],
-      direccion: ['', Validators.required],
-      correo: ['',Validators.required]
-     
-    });*/
-    this.Mascota = this._formBuilder.group({
-      mnombre: ['', Validators.required],
-      nregistro: ['', Validators.required],
+      registro_mascota: ['', Validators.required],
       tipo: ['', Validators.required],
       raza: ['', Validators.required],
       sexo: ['', Validators.required],
@@ -52,12 +36,12 @@ mascotaModel =
     });
 }
 
-//mascotaModel = new Mascota ("","","","","", undefined)
 
-submit(){
-  //console.log(this.Propietario.value);
-  //console.log(this.Mascota.value);
-  this.mascotasServicio.registrarMascota(this.mascotaModel).subscribe(() => {
+Onsubmit(){
+  //console.log(this.rmascota.value);
+
+
+  this.mascotasServicio.registrarMascota(this.rmascota.value).subscribe( data => {
 
     Swal.fire({
       position: 'top-end',
--- ProyectoDPS/src/app/servicios/mascotas/registro.service.ts
+++ ProyectoDPS/src/app/servicios/mascotas/registro.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Mascota} from '../../modelos/mascotas/mascota';
-import { environment } from '../../../environments/environment';
+import { ApiResponse } from '../../modelos/ApiResponse/api-response';
+import {Observable} from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,8 +11,8 @@ export class RegistroService {
   url = 'http://localhost:80/vetDPS/';
 
   constructor(private http: HttpClient) { }
-
-  registrarMascota(mascota: Mascota){
-    return this.http.post(`${this.url}/insertMascota.php`,mascota);
-  }
+registrarMascota(mascota: Mascota): Observable<ApiResponse>{
+  return this.http.post<ApiResponse>(`${this.url}insertMascota.php`,mascota);
+}
+ 
 }
